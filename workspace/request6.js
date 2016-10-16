594fe8a6269cdfaf9eb7d6b8b0f649b05e8fb7fe
henry <995821881@qq.com>
2016-10-16 12:35:19 +0800

    iconv-lite/.npmignore

--- workspace/request6.js
+++ workspace/request6.js
@@ -1,9 +1,10 @@
 var request = require('request');
 var fs = require('fs');
 var cheerio = require('cheerio');
+var iconv = require("iconv-lite");
 
 var options ={
-	url:"https://s.taobao.com/search?q=%E6%89%8B%E6%9C%BA&imgfile=&commend=all&ssid=s5-e&search_type=item&sourceId=tb.index&spm=a21bo.50862.201856-taobao-item.1&ie=utf8&initiative_id=tbindexz_20161014",
+	url:"http://search.dangdang.com/?key=%C4%DA%B4%E6%CC%F5&act=input",
 
 
 };
@@ -14,7 +15,14 @@ function callback(error,response,body){
 
 	if(!error && response.statusCode == 200){
 	
-	   fs.writeFile("./test6.html",body,"utf-8",function(err){
+
+	
+ 
+	var buff = iconv.encode(body,'GBK');
+	var b = iconv.decode(buff,"utf8");
+	console.log(b);
+
+	   fs.writeFile("./test6.html",b,"utf-8",function(err){
 	   
 		   if(!err){
 		   
